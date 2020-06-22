fb4880882d76615cc97b16019c95f68d833ba569
Yiru Wang Mac <wangyiru1@huawei.com>
2020-06-22 11:03:49 +0800

    change service file path to /run/pkgship_uwsgi

--- packageship/uWSGI_service.sh
+++ packageship/uWSGI_service.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 SYS_PATH=/etc/pkgship/
+OUT_PATH=/run/pkgship_uwsgi
+if [ ! -d "$OUT_PATH" ]; then
+        mkdir $OUT_PATH
+fi
 
 function get_config(){
     cat $SYS_PATH/package.ini | grep -E ^$2 | sed s/[[:space:]]//g | awk 'BEGIN{FS="="}{print $2}' > config_file
 }
 
 function create_config_file(){
-    echo "config type is: $config_type"
-    get_config "$config_type" "daemonize"
+    echo "config type is: $service"
+    get_config "$service" "daemonize"
     daemonize=$(cat config_file)
-    get_config "$config_type" "buffer-size"
+    get_config "$service" "buffer-size"
     buffer_size=$(cat config_file)
     wsgi_file_path=$(find /usr/lib/ -name "packageship")
-    if [ $config_type = "manage" -o $config_type = "all" ];then
-        get_config "$config_type" "write_port"
+    if [ $service = "manage" -o $service = "all" ];then
+        get_config "$service" "write_port"
         write_port=$(cat config_file)
-        get_config "$config_type" "write_ip_addr"
+        get_config "$service" "write_ip_addr"
         write_ip_addr=$(cat config_file)
         echo "manage.ini:"
         echo "[uwsgi]
@@ -24,14 +28,14 @@ module=packageship.manage
 wsgi-file=$wsgi_file_path/manage.py
 callable=app
 buffer-size=$buffer_size
-daemonize=$daemonize" > $SYS_PATH/manage.ini
-        cat $SYS_PATH/manage.ini
+daemonize=$daemonize" > $OUT_PATH/manage.ini
+        cat $OUT_PATH/manage.ini
     fi
     
-    if [ $config_type = "selfpkg" -o $config_type = "all" ];then
-        get_config "$config_type" "query_port"
+    if [ $service = "selfpkg" -o $service = "all" ];then
+        get_config "$service" "query_port"
         query_port=$(cat config_file)
-        get_config "$config_type" "query_ip_addr"
+        get_config "$service" "query_ip_addr"
         query_ip_addr=$(cat config_file)
         echo "selfpkg.ini:"
         echo "[uwsgi]
@@ -40,8 +44,8 @@ module=packageship.selfpkg
 wsgi-file=$wsgi_file_path/selfpkg.py
 callable=app
 buffer-size=$buffer_size
-daemonize=$daemonize" > $SYS_PATH/selfpkg.ini
-        cat $SYS_PATH/selfpkg.ini
+daemonize=$daemonize" > $OUT_PATH/selfpkg.ini
+        cat $OUT_PATH/selfpkg.ini
 
     fi
 
@@ -55,15 +59,15 @@ then
 fi
 
 if [ X$2 = X ];then
-    config_type="all"
+    service="all"
 elif [ $2 = "manage" -o $2 = "selfpkg" ];then
-    config_type=$2
+    service=$2
 else
     echo "can not phase the input of $2"
     exit 0
 fi
 
-create_config_file $config_type
+create_config_file $service
 
 if [ $1 = start ]
 then
@@ -73,8 +77,8 @@ then
 		echo "uwsgi is running!"
 		exit 0
 	else
-		uwsgi -d --ini /etc/pkgship/manage.ini
-		uwsgi -d --ini /etc/pkgship/selfpkg.ini
+		uwsgi -d --ini $OUT_PATH/manage.ini
+		uwsgi -d --ini $OUT_PATH/selfpkg.ini
 		echo "Start uwsgi service [OK], Please see the run log in $daemonize"
 	fi
 
@@ -84,8 +88,8 @@ elif [ $1 = stop ];then
 	echo "Stop uwsgi service [OK], Please see the run log in $daemonize"
 elif [ $1 = restart ];then
 	killall -9 uwsgi
-	uwsgi -d --ini /etc/pkgship/manage.ini
-	uwsgi -d --ini /etc/pkgship/selfpkg.ini
+	uwsgi -d --ini $OUT_PATH/manage.ini
+	uwsgi -d --ini $OUT_PATH/selfpkg.ini
 	echo "Restart uwsgi service [OK], Please see the run log in $daemonize"
 
 else
