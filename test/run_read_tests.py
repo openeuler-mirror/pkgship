daad84ddbbc71123f351d015407a2334d4e3ff58
stutao <1450026690@qq.com>
2020-07-03 09:40:16 +0800

    add test_install_depend.py and test_self_depend.py, at the same time, add the corresponding correct JSON return content,modify the running file and add the corresponding TestSuite to the execution file

--- test/run_read_tests.py
+++ test/run_read_tests.py
@@ -7,7 +7,8 @@ from test.test_module.packages_tests.test_packages import test_packages_suit
 from test.test_module.single_package_tests.test_get_singlepack import test_get_single_package_suit
 from test.test_module.repodatas_test.test_get_repodatas import test_get_repodatas_suit
 from test.test_module.dependent_query_tests.test_be_depend import test_be_depend_suit
-
+from test.test_module.dependent_query_tests.test_install_depend import test_install_depend_suit
+from test.test_module.dependent_query_tests.test_self_depend import test_self_depend_suit
 
 def execute_read():
     """Test case of reading data"""
@@ -16,7 +17,8 @@ def execute_read():
     test_get_single_package_suit()
     test_get_repodatas_suit()
     test_be_depend_suit()
-
+    test_install_depend_suit()
+    test_self_depend_suit()
 
 if __name__ == '__main__':
     execute_read()
