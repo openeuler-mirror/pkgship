73236af3e1ac6eacf65685157a43b0a605154ef6
gongzt <gong_zhengtang@163.com>
2020-08-10 09:33:19 +0800

    Data batch import command line development

--- packageship/pkgship.py
+++ packageship/pkgship.py
@@ -143,7 +143,7 @@ class PkgshipCommand(BaseCommand):
             ['package name', 'src name', 'version', 'database'])
 
         # Calculate the total width of the current terminal
-        self.columns = int(os.popen('stty size', 'r').read().split()[1])
+        # self.columns = int(os.popen('stty size', 'r').read().split()[1])
         self.params = []
 
     @staticmethod
@@ -199,6 +199,8 @@ class PkgshipCommand(BaseCommand):
         cls.register_command(SelfBuildCommand())
         cls.register_command(BeDependCommand())
         cls.register_command(SingleCommand())
+        cls.register_command(IssueCommand())
+        cls.register_command(ImportCommand())
         try:
             args = cls.parser.parse_args()
             args.func(args)
@@ -549,7 +551,14 @@ class AllPackageCommand(PkgshipCommand):
         self.table = self.create_table(
             ['packagenames', 'database', 'version', 'license'])
         self.params = [('-db', 'str', 'name of the database operated', '', 'store'),
-                       ('-remote', 'str', 'The address of the remote service', False, 'store_true')]
+                       ('-remote', 'str', 'The address of the remote service',
+                        False, 'store_true'),
+                       ('-pkgname', 'str',
+                        'Package name that needs fuzzy matching', '', 'store'),
+                       ('-maintainner', 'str', 'Maintainer\'s name', '', 'store'),
+                       ('-maintainlevel', 'str',
+                        'Maintain the level of data', '', 'store'),
+                       ]
 
     def register(self):
         """
@@ -564,6 +573,26 @@ class AllPackageCommand(PkgshipCommand):
         super(AllPackageCommand, self).register()
         self.parse.set_defaults(func=self.do_command)
 
+    def __parse_package(self, response_data, table_name):
+        """
+        Description: Parse the corresponding data of the package
+        Args:
+            response_data: http request response content
+        Returns:
+
+        Raises:
+
+        """
+        if response_data.get('code') == ResponseCode.SUCCESS:
+            package_all = response_data.get('data')
+            if isinstance(package_all, list):
+                for package_item in package_all:
+                    row_data = [package_item.get('name'), table_name, package_item.get(
+                        'version'), package_item.get('license')]
+                    self.table.add_row(row_data)
+        else:
+            print(response_data.get('msg'))
+
     def do_command(self, params):
         """
         Description: Action to execute command
@@ -576,7 +605,12 @@ class AllPackageCommand(PkgshipCommand):
         """
         self._set_read_host(params.remote)
         _url = self.read_host + \
-            '/packages?dbName={dbName}'.format(dbName=params.db)
+            '/packages?table_name={table_name}&query_pkg_name={pkg_name}& \
+            maintainner={maintainner}&maintainlevel={maintainlevel}'.format(
+                table_name=params.db,
+                pkg_name=params.pkgname,
+                maintainner=params.maintainner,
+                maintainlevel=params.maintainlevel)
         try:
             response = requests.get(_url)
         except ConnErr as conn_error:
@@ -585,7 +619,7 @@ class AllPackageCommand(PkgshipCommand):
         else:
             if response.status_code == 200:
 
-                self.parse_package(json.loads(response.text))
+                self.__parse_package(json.loads(response.text), params.db)
                 if self.table:
                     print(self.table)
             else:
@@ -611,9 +645,9 @@ class UpdatePackageCommand(PkgshipCommand):
         self.params = [
             ('packagename', 'str', 'Source package name', '', 'store'),
             ('db', 'str', 'name of the database operated', '', 'store'),
-            ('-m', 'str', 'Maintainers name', '', 'store'),
-            ('-l', 'int', 'database priority', 1, 'store'),
-            ('-t', 'str', 'package expiry date', '', 'store')
+            ('-maintainer', 'str', 'Maintainers name', '', 'store'),
+            ('-maintainlevel', 'int', 'database priority', 1, 'store'),
+            ('-endoflife', 'str', 'package expiry date', '', 'store')
         ]
 
     def register(self):
@@ -639,14 +673,14 @@ class UpdatePackageCommand(PkgshipCommand):
         Raises:
             ConnectionError: Request connection error
         """
-        _url = self.write_host + '/packages/packageInfo'
+        _url = self.write_host + '/lifeCycle/updatePkgInfo'
         try:
             response = requests.put(
-                _url, data=json.dumps({'srcname': params.packagename,
-                                       'tbname': params.db,
-                                       'maintainer': params.m,
-                                       'maintainlevel': params.l,
-                                       'end_time': params.t}),
+                _url, data=json.dumps({'pkg_name': params.packagename,
+                                       'table_name': params.db,
+                                       'maintainer': params.maintainer,
+                                       'maintainlevel': params.maintainlevel,
+                                       'end_of_life': params.endoflife}),
                 headers=self.headers)
         except ConnErr as conn_error:
             LOGGER.logger.error(conn_error)
@@ -1171,6 +1205,8 @@ class SingleCommand(PkgshipCommand):
             ('-db', 'str', 'name of the database operated', '', 'store'),
             ('-remote', 'str', 'The address of the remote service', False, 'store_true')
         ]
+        self.provides_table = self.create_table(['Symbol', 'Required by'])
+        self.requires_table = self.create_table(['Symbol', 'Provides by'])
 
     def register(self):
         """
@@ -1185,7 +1221,42 @@ class SingleCommand(PkgshipCommand):
         super(SingleCommand, self).register()
         self.parse.set_defaults(func=self.do_command)
 
-    def parse_package(self, response_data):
+    def __parse_package_detail(self, response_data):
+        """
+
+        """
+        _show_field_name = ('pkg_name', 'version', 'release', 'url', 'license', 'feature',
+                            'maintainer', 'maintainlevel', 'gitee_url', 'issue', 'summary',
+                            'description', 'buildrequired')
+        _package_detail_info = response_data.get('data')
+        _line_content = []
+        if _package_detail_info:
+            for key, value in _package_detail_info.items():
+                if value is None:
+                    value = ''
+                # buildrequired value
+                if isinstance(value, list):
+                    value = '、'.join(value)
+                if key in _show_field_name:
+                    _line_content.append('%-15s:%s' % (key, value))
+        for content in _line_content:
+            self.print_(content=content)
+
+    def __parse_provides(self, subpacks):
+        """
+
+        """
+        if subpacks and isinstance(subpacks, list):
+            for _subpack in subpacks:
+                pass
+
+    def __parse_requires(self, subpacks):
+        """
+
+        """
+        pass
+
+    def __parse_package(self, response_data):
         """
         Description: Parse the corresponding data of the package
         Args:
@@ -1195,25 +1266,17 @@ class SingleCommand(PkgshipCommand):
         Raises:
 
         """
-        show_field_name = ('sourceName', 'dbname', 'version',
-                           'license', 'maintainer', 'maintainlevel')
-        print_contents = []
+
         if response_data.get('code') == ResponseCode.SUCCESS:
-            package_all = response_data.get('data')
-            if isinstance(package_all, list):
-                for package_item in package_all:
-                    for key, value in package_item.items():
-                        if value is None:
-                            value = ''
-                        if key in show_field_name:
-                            line_content = '%-15s:%s' % (key, value)
-                            print_contents.append(line_content)
-                    print_contents.append('=' * self.columns)
+            self.__parse_package_detail(response_data)
+            try:
+                _subpacks = response_data['data']['subpack']
+                self.__parse_provides(_subpacks)
+                self.__parse_requires(_subpacks)
+            except KeyError as key_error:
+                LOGGER.logger.error(key_error)
         else:
             print(response_data.get('msg'))
-        if print_contents:
-            for content in print_contents:
-                self.print_(content=content)
 
     def do_command(self, params):
         """
@@ -1227,7 +1290,7 @@ class SingleCommand(PkgshipCommand):
         """
         self._set_read_host(params.remote)
         _url = self.read_host + \
-            '/packages/packageInfo?dbName={db_name}&sourceName={packagename}' \
+            '/packages/packageInfo?table_name={db_name}&pkg_name={packagename}' \
                    .format(db_name=params.db, packagename=params.packagename)
         try:
             response = requests.get(_url)
@@ -1236,7 +1299,7 @@ class SingleCommand(PkgshipCommand):
             print(str(conn_error))
         else:
             if response.status_code == 200:
-                self.parse_package(json.loads(response.text))
+                self.__parse_package(json.loads(response.text))
             else:
                 self.http_error(response)
 
@@ -1320,6 +1383,66 @@ class IssueCommand(PkgshipCommand):
         else:
             if response.status_code == 200:
                 self.parse_package(json.loads(response.text))
+            else:
+                self.http_error(response)
+
+
+class ImportCommand(PkgshipCommand):
+    """
+    Description: Import package information in the life cycle
+    Attributes:
+        parse: Command line parsing example
+        params: Command line parameters
+    """
+
+    def __init__(self):
+        """
+        Description: Class instance initialization
+        """
+        super(ImportCommand, self).__init__()
+
+        self.parse = PkgshipCommand.subparsers.add_parser(
+            'import', help='Import package information in the life cycle')
+        self.params = [
+            ('tablename', 'str', 'The name of the table to be created', '', 'store'),
+            ('filepath', 'str', 'Imported sqlite file path', '', 'store'),
+        ]
+
+    def register(self):
+        """
+        Description: Command line parameter injection
+
+        """
+        super(ImportCommand, self).register()
+        self.parse.set_defaults(func=self.do_command)
+
+    def do_command(self, params):
+        """
+        Description: Action to execute command
+        Args:
+            params: command lines params
+        Returns:
+
+        Raises:
+            ConnectionError: requests connection error
+        """
+        _url = self.write_host + '/lifeCycle/importdata'
+        try:
+            response = requests.post(
+                _url,
+                data=json.dumps(
+                    {'filepath': params.filepath, 'tablename': params.tablename}),
+                headers=self.headers)
+        except ConnErr as conn_error:
+            LOGGER.logger.error(conn_error)
+            print(str(conn_error))
+        else:
+            if response.status_code == 200:
+                _response_content = json.loads(response.text)
+                if _response_content.get('code') == ResponseCode.SUCCESS:
+                    print('import success')
+                else:
+                    print('import failure')
             else:
                 self.http_error(response)
 
