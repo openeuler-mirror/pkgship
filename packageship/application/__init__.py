6d508ac49c49371af7c137d8fd525af5cadea471
tomtao <1450026690@qq.com>
2020-08-30 00:52:33 +0800

    复原application的init文件

--- packageship/application/__init__.py
+++ packageship/application/__init__.py
@@ -7,7 +7,6 @@ import threading
 from flask import Flask
 from flask_session import Session
 from flask_apscheduler import APScheduler
-from apscheduler.schedulers.background import BackgroundScheduler
 from packageship import system_config
 from packageship.application.settings import Config
 from packageship.libs.log import setup_log
@@ -55,7 +54,7 @@ def init_app(operation):
     app.config.from_object(Config)
 
     # Register a scheduled task
-    scheduler = APScheduler(scheduler=BackgroundScheduler(timezone='Asia/Shanghai'))
+    scheduler = APScheduler()
     scheduler.init_app(app)
     scheduler.start()
 
