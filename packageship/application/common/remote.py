b51cd459a6ee49d4b8a277b7499bfa0dfefa782b
gongzhengtang <gong_zhengtang@163.com>
2021-01-08 09:48:54 +0800

    http请求、失败重试，各种压缩包格式的文件解压

--- packageship/application/common/remote.py
+++ packageship/application/common/remote.py
@@ -10,72 +10,96 @@
 # PURPOSE.
 # See the Mulan PSL v2 for more details.
 # ******************************************************************************/
-"""
-    common remote service class
-"""
+
+import requests
+from requests.exceptions import RequestException, HTTPError
+from retrying import retry
+from packageship.libs.log import LOGGER
+
 
 class RemoteService:
     """
-        common remote service class
+    HTTP request service
     """
+
+    def __init__(self, max_delay=1000):
+        self._retry = 3
+        self._max_delay = max_delay
+        self._body = None
+        self._response = None
+
     @property
     def status_code(self):
-        """
-            get the status code
-        """
-        pass
+        """status code of the response"""
+        if self._response is None:
+            return requests.codes["internal_server_error"]
+        return self._response.status_code
 
     @property
     def content(self):
-        """
-            undecoded original content
-        """
-        pass
+        """ original content of the response """
+        return self._response.content if self._response else None
 
     @property
     def text(self):
-        """
-            response content after decode
-        """
-        pass
+        """content of the decoded response"""
+        return self._response.content.decode('utf-8') if self._response else None
 
     @property
     def response_headers(self):
-        """
-         get response heaader
-        """
+        """request header for the response"""
         pass
 
-    @classmethod
-    def dispatch(cls, method, url, **kwargs):
-        """
-            diff request methods are executed by mapping
-            :param method: request method， GET、 POST 、PUT 、DELETE
-            :param url: remote download url
-            :param kwargs: related param for remoting requests
-            :param kwargs.request_body: request body content
-        """
-        pass
+    def _dispatch(self, method, url, **kwargs):
+        """Request remote services in different ways
 
-    def request(self, url, method, request_body=None, retry=3, **kwargs):
+        :param method: request method， GET、 POST 、PUT 、DELETE
+        :param url:Remote request address
+        :param kwargs:parameters associated with the request
         """
-            request remote service
-        :param url: remote url
-        :param method: request method
-        :param request_body:reuqest content
-        :param retry:default times for failed retrying
-        :param kwargs: related param for request
+        @retry(stop_max_attempt_number=self._retry, stop_max_delay=self._max_delay)
+        def http(url):
+            response = method(url, **kwargs)
+            if response.status_code != requests.codes["ok"]:
+                _msg = "There is an exception with the remote service [%s]，" \
+                    "Please try again later.The HTTP error code is：%s" % (url, str(
+                        response.status_code))
+                raise HTTPError(_msg)
+            return response
+
+        method = getattr(self, method)
+        if method is None:
+            raise RequestException("")
+        try:
+            self._response = http(url)
+        except HTTPError as error:
+            raise RequestException("") from error
+
+    def request(self, url, method, body=None, max_retry=3, **kwargs):
+        """ Request a remote http service
+
+        :param url: http service address
+        :param method: mode of request ,only GET、 POST、 DELETE、 PUT is supported
+        :param body: Request body content
+        :param max_retry: The number of times the request failed to retry
+        :param kwargs: Request the relevant parameters
         """
-        pass
+        self._retry = max_retry
+        self._body = body
+        try:
+            self._dispatch(method=method, url=url, **kwargs)
+        except RequestException as error:
+            LOGGER.error(error)
 
     def get(self, url, **kwargs):
+        """ HTTP get request method
+        :param kwargs: requests parameters
         """
-        http get request
-        """
-        pass
+        response = requests.get(url=url, **kwargs)
+        return response
 
     def post(self, url, **kwargs):
+        """HTTP post request method
         """
-        http post request
-        """
-        pass
+        response = requests.post(url=url, data=self._body, **kwargs)
+        return response
