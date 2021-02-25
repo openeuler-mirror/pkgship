6d40acbdb796427190fe04be3ef4108c1796f42c
zt <1450026690@qq.com>
2021-02-25 21:18:16 +0800

    添加依赖查询分发方法

--- packageship/application/core/depend/__init__.py
+++ packageship/application/core/depend/__init__.py
@@ -10,3 +10,62 @@
 # PURPOSE.
 # See the Mulan PSL v2 for more details.
 # ******************************************************************************/
+from .be_depend import BeDepend
+from .build_depend import BuildDepend
+from .install_depend import InstallDepend
+from .self_depend import SelfDepend
+
+
+class DispatchDepend:
+    """
+        Factory method, distributing operations of different depend search
+    """
+
+    def __init__(self):
+        self._kwargs = dict()
+
+    def _selfdep(self):
+
+        _self_depend = SelfDepend(
+            db_list=self._kwargs["parameter"]["db_priority"])
+        _self_depend(**self._kwargs)
+        return _self_depend
+
+    def _installdep(self):
+        _install_depend = InstallDepend(
+            db_list=self._kwargs["parameter"]["db_priority"])
+        _install_depend(**self._kwargs)
+        return _install_depend
+
+    def _builddep(self):
+        _build_depend = BuildDepend(
+            db_list=self._kwargs["parameter"]["db_priority"])
+        _build_depend(**self._kwargs)
+        return _build_depend
+
+    def _bedep(self):
+        _be_depend = BeDepend(**self._kwargs)
+        _be_depend(**self._kwargs)
+        return _be_depend
+
+    @classmethod
+    def execute(cls, **kwargs):
+        """Execute the map to return the dependent instance
+        """
+        
+        def dispatch():
+            depend_type = kwargs["depend_type"]
+            method = getattr(dispatch_cls, "_" + depend_type, None)
+            if not method:
+                raise AttributeError(
+                    "Queries that do not support dependencies are not supported")
+
+            return method()
+        if "depend_type" not in kwargs:
+            raise ValueError(
+                "Missing the necessary query parameter 'depend_type' .")
+        dispatch_cls = cls()
+
+        setattr(dispatch_cls, "_kwargs", kwargs)
+
+        return dispatch()
