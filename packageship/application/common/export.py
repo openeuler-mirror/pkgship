df32724493e5cd2cac378e28b257954389397ab7
e <jiangpengju1@yeah.net>
2021-02-26 16:03:07 +0800

    压缩的csv文件代码上传

--- packageship/application/common/export.py
+++ packageship/application/common/export.py
@@ -10,32 +10,71 @@
 # PURPOSE.
 # See the Mulan PSL v2 for more details.
 # ******************************************************************************/
+"""
+Send file stream
+"""
+
+import os
+import shutil
+import zipfile
+from io import BytesIO
+from packageship.libs.log import LOGGER
 
 
-class Download:
+class CompressIo():
+    """
+    Public methods for sending and deleting files
+    """
 
-    def export(self, func_name, file_type="csv", **kwargs):
+    def _delete_file_path(self, folder_path):
         """
-            export cvs or excel files
-            :param func_name: which types of function exported (packages, installdepend, builddepend,etc）
-            :param file_type: exported files type, only support csv or excel
-            :param kwargs: accessory functionality, such as binary streams or file paths
-            :param kwargs.binary_stream: a binary streams would be exxported
-            :param kwargs.save_path: exported path
-            :param kwargs.temporary: temp files path
-        """
-        pass
+        Delete folder
+        Args:
+            folder_path: Folder path
 
-    def _set_response_header(self, response, file):
+        Returns:
+            None
         """
-            :param response: response content
-            :param file: file name
-        """
-        pass
+        try:
+            if folder_path and os.path.exists(folder_path):
+                shutil.rmtree(folder_path)
+        except (IOError, OSError) as error:
+            LOGGER.error(error)
+            raise IOError("Failed to delete folder")
 
-    @property
-    def response(self):
+    def _send_bytes_io(self, folder_path):
         """
-            response body
+        Send a compressed file
+        Args:
+            folder_path: The file path
+        Returns:
+            memory_file: File stream
         """
-        pass
+        file_list = os.listdir(folder_path)
+        memory_file = BytesIO()
+        with zipfile.ZipFile(memory_file, "w", zipfile.ZIP_DEFLATED) as zip_file:
+            for _file in file_list:
+                with open(os.path.join(folder_path, _file), 'rb') as file_content:
+                    zip_file.writestr(_file, file_content.read())
+        memory_file.seek(0)
+        return memory_file
+
+    def send_memory_file(self, folder_path):
+        """
+        Main method of sending files
+        Args:
+            folder_path: The file path
+        Returns:
+            zip_file: File stream
+
+        Raises:
+            IOError: IOError
+        """
+        try:
+            memory_file = self._send_bytes_io(folder_path)
+            return memory_file
+        except (OSError, IOError) as error:
+            LOGGER.error(error)
+            raise IOError("File sending failed")
+        finally:
+            self._delete_file_path(folder_path)
