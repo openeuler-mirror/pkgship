4c2c565a4292a4499d85479bca75235f0c669d2f
tomtao <1450026690@qq.com>
2021-03-01 11:50:51 +0800

    测试CI捕获用例failures

--- test/test_module/test_basedepend/test_dispatch.py
+++ test/test_module/test_basedepend/test_dispatch.py
@@ -73,9 +73,9 @@ class TestDispatch(unittest.TestCase):
         }
 
         mock_exists.return_value = True
-        a = 1/0
         insd = DispatchDepend.execute(**install_params)
-        self.assertTrue(isinstance(insd, InstallDepend))
+        # self.assertTrue(isinstance(insd, InstallDepend))
+        self.assertTrue(isinstance(insd, BuildDepend))
 
     @mock.patch.object(Redis, "exists")
     @mock.patch.object(Redis, "hgetall")
