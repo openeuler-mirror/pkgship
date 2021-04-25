c7a0c5ef4510df29e135344cffe72d6565ee557f
MementoMoriCheng <1003350679@qq.com>
2021-04-25 11:45:07 +0800

    build depend 测试用例优化

--- test/coverage_count.py
+++ test/coverage_count.py
@@ -46,9 +46,7 @@ if __name__ == "__main__":
     args = sys.argv
     cov.start()
     test_case_files = [
-                       os.path.join(TEST_CASE_PATH, "cli/"),
-                       os.path.join(TEST_CASE_PATH, "graph/"),
-                       os.path.join(TEST_CASE_PATH, "unpack/")
+                       os.path.join(TEST_CASE_PATH, "cli/")
                        ]
 
     errors = []
