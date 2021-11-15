661fb358b167eb85b2dacf1e34ecaa2fe6ea935a
haml-707 <845831435@qq.com>
2021-11-15 16:29:17 +0800

    update atom
    update atom

--- web-ui/src/components/footer.vue
+++ web-ui/src/components/footer.vue
@@ -1,5 +1,9 @@
 <template>
     <div class="footer-wrapper">
+        <div class="atom">
+            <p>{{ footer.atomText }}</p>
+            <img src="@/assets/images/atom.png" alt="">
+        </div>
         <div class="footer-content">
             <div class="footer-left">
                 <a href="https://gitee.com/openeuler/pkgship" target="_blank">
@@ -21,6 +25,7 @@ export default {
     data() {
         return {
             footer: {
+                atomText:'openEuler 是由开放原子开源基金会（OpenAtom Foundation）孵化及运营的开源项目',
                 leftLogo: 'openEuler',
                 mail: 'contact@openeuler.org',
                 copyright: '版权所有 © 2020 openeuler 保留一切权利',
@@ -46,15 +51,42 @@ export default {
 
 <style lang="less" scoped>
 .footer-wrapper {
-    height: 160px;
+    height: 490px;
     background-color: #111;
     @media (max-width: 1000px) {
-        height: 280px;
+        height: 460px;
+    }
+    .atom {
+        height: 332px;
+        text-align: center;
+        overflow: hidden;
+        img {
+          width: 380px;
+          height: 81px;
+        }
+        p {
+          margin: 88px 0 50px 0;
+          color: #fff;
+          font-size: 20px;
+        }
+    }
+    @media screen and (max-width: 1000px) {
+     .atom {
+      padding: 0 50px;
+      height: 180px;
+      img {
+        width: 191px;
+        height: 41px;
+      }
+      p {
+        font-size: 12px;
+        margin: 40px 0 20px 0;
+      }
+     }
     }
     .footer-content {
         .ff-xih();
         margin: 0 auto;
-        height: 100%;
         width: 1200px;
         display: flex;
         justify-content: space-between;
@@ -62,13 +94,13 @@ export default {
             flex-direction: column;
             align-items: center;
             width: 100%;
-
+            height: 280px;
         }
         .footer-left {
             @media (max-width: 1000px) {
                 height: 50%;
             }
-            height: 100%;
+            height: 160px;
             display: flex;
             flex-direction: column;
             justify-content: center;
@@ -84,12 +116,13 @@ export default {
                 align-items: unset;
                 padding-bottom: 67px;
             }
-            height: 100%;
+            height: 160px;
             display: flex;
             flex-direction: column;
             justify-content: center;
             align-items: flex-end;
             .footer-copyright {
+                text-align: center;
                 color: #fff;
                 margin-top: 21px;
                 .fz12();
