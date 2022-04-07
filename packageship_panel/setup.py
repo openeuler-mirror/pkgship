da81d0237975cc5459cce9769ad86f4fe0297c7f
gongzt <gong_zhengtang@163.com>
2022-04-07 15:47:04 +0800

    pkgship子包panel和packageship的目录调整

---
+++ packageship_panel/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# ******************************************************************************
+# Copyright (c) Huawei Technologies Co., Ltd. 2020-2020. All rights reserved.
+# licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#     http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# PURPOSE.
+# See the Mulan PSL v2 for more details.
+# ******************************************************************************/
+"""
+Package management program installation configuration
+file for software packaging
+"""
+import os
+from setuptools import setup, find_packages
+from distutils.sysconfig import get_python_lib
+
+PACKAGESHIP_PANEL_TEMPLATE_PATH = os.path.join(
+    get_python_lib(), "packageship_panel", "obs_info_template.csv"
+)
+
+setup(
+    name="packageship-panel",
+    version="1.0",
+    packages=find_packages(),
+    requires=[
+        "packageship",
+        "aiohttp(==3.8.1)",
+        "APScheduler(==3.8.1)",
+        "lxml(==4.6.3)",
+    ],
+    license="The software package builds the data panel",
+    author="gongzhengtang",
+    data_files=[
+        ("/usr/bin", ["pkgship-paneld", "pkgship-panel"]),
+        ("/lib/systemd/system/", ["pkgship-panel.service"]),
+        (
+            "/etc/pkgship/",
+            ["timed_task.yaml", "sig_mentor.yaml"],
+        ),
+        (PACKAGESHIP_PANEL_TEMPLATE_PATH, ["obs_info_template.csv"]),
+    ],
+    zip_safe=False,
+)
